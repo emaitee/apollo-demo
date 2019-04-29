8f1052726f0305981142599b6919e23d01ccc85a
Mustapha Issa Toyin <issatoyin@gmail.com>
2019-04-30 00:47:01 +0100

    modify readme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { gql } from 'apollo-boost'
+import { Query } from 'react-apollo'
+import { fontSize, colors } from './styles'
+import numeral from 'numeral'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const ExchangeRateQuery = gql
+
+const styles = {
+  bigContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1
+  },
+  container: {
+    width: "100%",
+    padding: 20
+  },
+  currencyWrapper: {
+    padding: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: colors.teal
+  },
+  currency: {
+    fontSize: fontSize.medium,
+    fontWeight: "100",
+    color: colors.grey,
+    letterSpacing: 4
+  },
+  heading: {
+    fontSize: fontSize.large,
+    fontWeight: "200",
+    color: colors.white,
+    letterSpacing: 6
+  }
 }
 
-export default App;
+function List({ currency: currentCurrency, onCurrencyChange }) {
+  let query = gql`
+    query rates($currency: String!) {
+      rates(currency: $currency) {
+        currency
+        rate
+      }
+    }
+  `
+  return (
+    <Query 
+      query={query} 
+      variables={{currency: currentCurrency}}>
+        {({ loading, error, data }) => {
+          if(loading) return <p>Loading...</p>
+          if(error) return <p>Something went wrong</p>
+
+          return (
+            <div style={styles.container}>
+              { data.rates.filter(({currency}) => currency !== currentCurrency && ["USD", "BTC", "LTC", "EUR", "JPY", "ETH"].includes(currency))
+                .map(({ currency, rate }, i, rateArr) => (
+                  <div style={[styles.currencyWrapper, i === rateArr.length - 1 && { borderBottomWidth:0}]}
+                    key={currency}>
+                      <p style={styles.currency}>{currency}</p>
+                      <p style={styles.currency}>
+                        {rate > 1 ? numeral(rate).format("0,0.00") : rate}
+                      </p>
+                  </div>
+                ))
+              }
+            </div>
+          );
+        }}
+    </Query>
+  )
+}
+
+export default class ExchangeRate extends React.Component {
+  state = {
+    currency: "USD"
+  };
+
+  onCurrencyChange = currency => this.setState(() => ({ currency }))
+
+  render() {
+  const { currency } = this.state;
+  
+  return (
+    <div style={styles.bigContainer}>
+      <p style={styles.heading}>{`1 ${this.state.currency}`}</p>
+      <List 
+        currency={currency} 
+        onCurrencyChange={this.onCurrencyChange}
+      />
+    </div>
+  )
+}
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { colors } from './styles'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    uri: 'https://48p1r2roz4.sse.codesandbox.io'
+})
+
+render(
+    <ApolloProvider client={client}>
+        <div style={{ height: "100vh", backgroundColor: colors.darkBlue }}>
+            <App />
+        </div>
+    </ApolloProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
